c712d0d7a05068ff92716441e5a8929b3213ca68
Kim Ipsen (BUVM-STIL) <kim.ipsen@stil.dk>
2024-05-22 12:53:49 +0200

    Use gitversion instead of octoversion

--- build/Build.Github.cs
+++ build/Build.Github.cs
@@ -4,6 +4,7 @@ using Nuke.Common;
 using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Git;
 using Nuke.Common.Tools.GitHub;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Tools.OctoVersion;
 
 using Octokit;
@@ -22,7 +23,7 @@ namespace Groupify.Build;
 public partial class Build
 {
     GitHubActions GitHubActions => GitHubActions.Instance;
-    [OctoVersion(AutoDetectBranch = true)] readonly OctoVersionInfo OctoVersionInfo;
+    [GitVersion]readonly GitVersion GitVersion;
     [Nuke.Common.Parameter] readonly string GitHubUser = GitHubActions.Instance?.RepositoryOwner;
     [Nuke.Common.Parameter, Secret] readonly string GitHubToken;
 
@@ -31,23 +32,38 @@ public partial class Build
         // .DependsOn(Test, MutationTests)
         .Executes(async () =>
         {
+            //https://anktsrkr.github.io/post/manage-your-package-release-using-nuke-in-github/
             var credentials = new Credentials(GitHubActions.Token);
+            var (owner, name) = (Repository.GetGitHubOwner(), Repository.GetGitHubName());
+
             GitHubTasks.GitHubClient = new GitHubClient(
                 new ProductHeaderValue(nameof(NukeBuild)),
                 new InMemoryCredentialStore(credentials));
 
-            NewTag newTag = new()
+            string releaseTag = GitVersion.NuGetVersionV2;
+            NewRelease newRelease = new(releaseTag)
             {
-                Tag = OctoVersionInfo.FullSemVer,
-                Object = Repository.Commit,
-                Type = TaggedType.Commit,
-                Tagger = new Committer("Kim Ipsen", "kim.ipsen@outlook.dk", DateTimeOffset.UtcNow)
+                TargetCommitish = GitVersion.Sha,
+                Draft = true,
+                Name = $"v{releaseTag}",
+                Prerelease = !string.IsNullOrWhiteSpace(GitVersion.PreReleaseTag),
+                Body = "",
             };
-            await GitHubTasks.GitHubClient.Git.Tag.Create("Kim Ipsen", OctoVersionInfo.FullSemVer, newTag);
 
-            // NewRelease newRelease = new(OctoVersionInfo.FullSemVer);
+            Release createdRelease = await GitHubTasks
+                .GitHubClient
+                .Repository
+                .Release
+                .Create(owner, name, newRelease)
+            ;
 
-            // await GitHubTasks.GitHubClient.Repository.Release.Create(GitHubUser, GitHubToken, newRelease);
+            // TODO: upload files
+
+            await GitHubTasks
+                .GitHubClient
+                .Repository
+                .Release
+                .Edit(owner, name, createdRelease.Id, new ReleaseUpdate{ Draft = false });
         });
 
     // https://blog.raulnq.com/github-packages-publishing-nuget-packages-using-nuke-with-gitversion-and-github-actions#heading-create-a-github-action-workflow
